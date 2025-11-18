81333f21ac5c51958983c99259b86368a06a96eb
Jeannie-Shackett <jeannieshackett1@gmail.com>
2025-11-17 19:45:55 -0600

    Began working on the drive base for Helix as well as the intake base

---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,26 @@
+// package frc.robot.subsystems;
+
+// import static edu.wpi.first.units.Units.*;
+
+// import java.util.function.BooleanSupplier;
+
+// import javax.swing.plaf.RootPaneUI;
+
+// import org.littletonrobotics.junction.AutoLogOutput;
+// import org.opencv.features2d.FlannBasedMatcher;
+
+// import com.ctre.phoenix.led.CANdle;
+// import com.ctre.phoenix6.SignalLogger;
+// import com.ctre.phoenix6.configs.CANcoderConfiguration;
+// import com.ctre.phoenix6.CANdiConfiguration;
+// import com.ctre.phoenix6.ClosedLoopGeneralConfigs;
+// import com.ctre.phoenix6.CommutationConfigs;
+// import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+// import com.ctre.phoenix6.configs.FeedbackConfigs;
+// import com.ctre.phoenix6.MagnetSensorConfigs;
+// import com.ctre.phoenix6.MotionMagicConfigs;
+// import com.ctre.phoenix6.MotorOutputConfigs;
+
+// public class IntakeSubsystem {
+
+// }
